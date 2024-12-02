0a5e48298daadf5f881e1d66147c51440da43d85
mreinhar2 <marius.reinhardt@sva.de>
2024-12-02 17:27:30 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # funny-xmas
 Funny xmas in flask
+
+## How to run   
+```
+apt install python3-flask
+python3 app.py
+```
---
+++ static/tree_animation.js
@@ -0,0 +1,4 @@
+// Optional additional interactivity or fun animations can be added here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Funny Christmas Tree with Santa is ready!");
+});
