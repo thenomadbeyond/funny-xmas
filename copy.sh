14fcc1365714216c2040b9c779c91d328e22ef36
thenomadbeyond <hello@marius-reinhardt.de>
2024-12-02 18:36:57 +0100

    changed index.html

--- copy.sh
+++ copy.sh
@@ -2,5 +2,5 @@
 rm -R templates && rm -R static && rm app.py
 mkdir templates && mkdir static 
 cd templates && wget https://raw.githubusercontent.com/thenomadbeyond/funny-xmas/refs/heads/main/templates/index.html
-cd ../static/ && wget https://github.com/thenomadbeyond/funny-xmas/blob/main/static/style.css && wget https://raw.githubusercontent.com/thenomadbeyond/funny-xmas/refs/heads/main/static/tree_animation.js
+cd ../static/ && wget https://raw.githubusercontent.com/thenomadbeyond/funny-xmas/refs/heads/main/static/style.css && wget https://raw.githubusercontent.com/thenomadbeyond/funny-xmas/refs/heads/main/static/tree_animation.js
 cd .. && wget https://raw.githubusercontent.com/thenomadbeyond/funny-xmas/refs/heads/main/app.py
